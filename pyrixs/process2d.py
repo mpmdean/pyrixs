3f29b5cb6167325d1179f2740aaeb9ccddb6c264
Mark <mpmdean@gmail.com>
2017-01-07 12:25:01 -0500

    globals removed from process2d
    Image_GUI broken
    Docustring not yet in sphinx format

--- pyrixs/process2d.py
+++ pyrixs/process2d.py
@@ -20,14 +20,18 @@ import lmfit, os, glob
 
 from pyrixs import loaddata
 
-global Image
-Image = OrderedDict({
-'photon_events' : np.array([]),
-'name' : '',
-'curvature' : np.array([0, 0, 500]),
-'image_meta' : '',
-'spectrum' : np.array([])
-})
+CONSTANT_OFFSET = 500
+
+# Delete this entirely.
+# Instead all functions take in the inputs they need and return their results.
+#global Image
+#Image = OrderedDict({
+#'photon_events' : np.array([]),
+#'name' : '',
+#'curvature' : np.array([0, 0, 500]),
+#'image_meta' : '',
+#'spectrum' : np.array([])
+#})
 
 def get_all_image_names(search_path):
     """Returns list of image names meeting the folder search term."""
@@ -35,16 +39,23 @@ def get_all_image_names(search_path):
     return [path.split('/')[-1] for path in paths]
 
 def load_image(search_path, selected_image_name):
-    """Read image into Image['photon_events']
+    """Return image based on file location
 
-    Arguments:
-    search_path -- string that defines folder containing images
-    selected_image_name -- list of image filenames to load
+    Parameters
+    ----------
+    search_path : string
+        defines folder containing images
+    selected_image_name : list
+        image filenames to load
+
+    Returns
+    ----------
+    photo_events : array
+        two column x, y photon location coordinates
     """
-    global Image
     filename = os.path.join(os.path.dirname(search_path), selected_image_name)
-    Image['photon_events'] = loaddata.getimage(filename)
-    Image['name'] = selected_image_name
+    photon_events = loaddata.getimage(filename)
+    return photon_events
 
 def make_fake_image():
     """Return a fake list of photon events."""
@@ -54,7 +65,7 @@ def make_fake_image():
     xvalues = 2**11 * np.random.rand(len(yvalues))
     return np.vstack((xvalues, yvalues-xvalues*.02)).transpose()
 
-def plot_image(ax1, alpha=0.5, s=1):
+def plot_image(ax1, photon_events, alpha=0.5, s=1):
     """Plot the image composed of an event list
 
     Arguments:
@@ -64,16 +75,15 @@ def plot_image(ax1, alpha=0.5, s=1):
     """
     plt.sca(ax1)
     ax1.set_axis_bgcolor('black')
-    photon_events = Image['photon_events']
     plt.scatter(photon_events[:,0], photon_events[:,1], c='white',
             edgecolors='white', alpha=alpha, s=s)
-    plt.title(Image['name'])
-    plt.xlim([-100, 1700])
+    #plt.title(Image['name'])
+    #plt.xlim([-100, 1700])
 
-def plot_curvature(ax1):
+def plot_curvature(ax1, curvature, photon_events):
     """ Plot a red line defining curvature on ax1"""
-    x = np.arange(np.nanmax(Image['photon_events'][:,0]))
-    y = poly(Image['curvature'])
+    x = np.arange(np.nanmax(photon_events[:,0]))
+    y = poly(curvature)
     curvature_line = plt.plot(x, y, 'r-', hold=True)
 
 def poly(x, p2, p1, p0):
@@ -101,25 +111,24 @@ def fit_poly(x_centers, offsets):
     poly_model = lmfit.Model(poly)
     params = poly_model.make_params()
     params['p0'].value = offsets[0]
-    for i, arg in enumerate(['p2', 'p1']):
-        params[arg].value = Image['curvature'][i]
-
+    params['p1'].value = 0.
+    params['p2'].value = 0.
     result = poly_model.fit(offsets, x=x_centers, params=params)
+    if not result.success:
+        print("Fitting failed")
+    return result
 
-    if result.success:
-        curvature_values = np.array([result.best_values[arg] for arg in ['p2', 'p1', 'p0']])
-        return curvature_values
 
-def fit_resolution(xmin=-np.inf, xmax=np.inf):
-    """Fit a Gaussian model to Image['spectrum'] in order to determine resolution_values.
+def fit_resolution(spectrum, xmin=-np.inf, xmax=np.inf):
+    """Fit a Gaussian model to ['spectrum'] in order to determine resolution_values.
 
     Arguments:
     xmin, xmax -- define range data to be used
     """
-    allx = Image['spectrum'][:,0]
+    allx = spectrum[:,0]
     choose = np.logical_and(allx>xmin, allx<=xmax)
     x = allx[choose]
-    y = Image['spectrum'][:,1][choose]
+    y = spectrum[:,1][choose]
 
     GaussianModel = lmfit.Model(gaussian)
     params = GaussianModel.make_params()
@@ -141,7 +150,7 @@ def bin_edges_centers(minvalue, maxvalue, binsize):
     centers = (edges[:-1] + edges[1:])/2
     return edges, centers
 
-def get_curvature_offsets(binx=64, biny=1):
+def get_curvature_offsets(photon_events, binx=64, biny=1):
     """ Determine the offests that define the isoenergetic line.
     This is determined as the maximum of the cross correlation function with
     a reference taken from the center of the image.
@@ -156,8 +165,8 @@ def get_curvature_offsets(binx=64, biny=1):
     offests -- np.array of row offsets defining curvature. This is referenced
                 to the center of the image.
     """
-    x = Image['photon_events'][:,0]
-    y = Image['photon_events'][:,1]
+    x = photon_events[:,0]
+    y = photon_events[:,1]
     x_edges, x_centers = bin_edges_centers(np.nanmin(x), np.nanmax(x), binx)
     y_edges, y_centers = bin_edges_centers(np.nanmin(x), np.nanmax(y), biny)
 
@@ -172,50 +181,47 @@ def get_curvature_offsets(binx=64, biny=1):
 
     return x_centers, offsets - offsets[offsets.shape[0]//2]
 
-def fit_curvature(binx=32, biny=1):
-    """Get offsets, fit them and return curvature_values
+def fit_curvature(photon_events, binx=32, biny=1, CONSTANT_OFFSET=500):
+    """Get offsets, fit them and return polynomial that defines the curvature
 
     Arguments:
     binx/biny -- width of columns/rows binned together prior to computing convolution
     """
-    global Image
-    x_centers, offsets = get_curvature_offsets(binx=binx, biny=biny)
-    curvature_values = fit_poly(x_centers, offsets)
-    curvature_values[-1] = Image['curvature'][-1]
-    return curvature_values
+    x_centers, offsets = get_curvature_offsets(photon_events, binx=binx, biny=biny)
+    result = fit_poly(x_centers, offsets)
+    curvature = np.array([result.best_values['p2'], result.best_values['p1'],
+                            CONSTANT_OFFSET])
+    return curvature
 
-def plot_curvature(ax1):
+def plot_curvature(ax1, curvature, photon_events):
     """ Plot a red line defining curvature on ax1"""
     plt.sca(ax1)
-    x = np.arange(np.nanmax(Image['photon_events'][:,0]))
-    y = poly(x, *Image['curvature'])
-    curvature_line = plt.plot(x, y, 'r-', hold=True)
+    x = np.arange(np.nanmax(photon_events[:,0]))
+    y = poly(x, *curvature)
+    return plt.plot(x, y, 'r-', hold=True)
 
-def extract(biny=1.):
+def extract(photon_events, curvature, biny=1.):
     """Apply curvature to photon events to create pixel versus intensity spectrum
 
     Arguments:
     biny -- width of rows binned together in histogram"""
-    global Image
-    x = Image['photon_events'][:,0]
-    y = Image['photon_events'][:,1]
-    corrected_y = y - poly(x, Image['curvature'][0], Image['curvature'][1], 0.)
+    x = photon_events[:,0]
+    y = photon_events[:,1]
+    corrected_y = y - poly(x, curvature[0], curvature[1], 0.)
     pix_edges, pix_centers = bin_edges_centers(np.nanmin(corrected_y),
                                             np.nanmax(corrected_y), biny)
     I, _ = np.histogram(corrected_y, bins=pix_edges)
     spectrum = np.vstack((pix_centers, I)).transpose()
-    Image['spectrum'] = spectrum
     return spectrum
 
-def plot_resolution(ax2):
+def plot_resolution(ax2, spectrum):
     """ Plot blue points defining the spectrum on ax2"""
     plt.sca(ax2)
-    spectrum = Image['spectrum']
     spectrum_line = plt.plot(spectrum[:,0], spectrum[:,1], 'b.')
     plt.xlabel('pixels')
     plt.ylabel('Photons')
 
-def plot_resolution_fit(ax2, resolution_values, xmin=None, xmax=None):
+def plot_resolution_fit(ax2, spectrum, resolution, xmin=None, xmax=None):
     """Plot the gaussian fit to the resolution function
 
     Arguments:
@@ -225,12 +231,12 @@ def plot_resolution_fit(ax2, resolution_values, xmin=None, xmax=None):
     """
     plt.sca(ax2)
     if xmin == None:
-        xmin = np.nanmin(Image['spectrum'][:,0])
+        xmin = np.nanmin(spectrum[:,0])
     if xmax == None:
-        xmax = np.nanmax(Image['spectrum'][:,0])
+        xmax = np.nanmax(spectrum[:,0])
     x = np.linspace(xmin, xmax, 10000)
-    y = gaussian(x, *resolution_values)
-    resolution_line = plt.plot(x, y, 'r-', hold=True)
+    y = gaussian(x, *resolution)
+    return plt.plot(x, y, 'r-', hold=True)
 
 def run_test(search_path='../test_images/*.h5'):
     """Run at test of the code.
@@ -241,29 +247,29 @@ def run_test(search_path='../test_images/*.h5'):
     """
     try:
         selected_image_name = get_all_image_names(search_path)[0]
-        load_image(search_path, selected_image_name)
+        photon_events = load_image(search_path, selected_image_name)
     except (KeyError, IndexError) as e:
         print("No data found: Simulate an image")
-        Image['photon_events'] = make_fake_image()
-        Image['name'] = 'fake data'
-    curvature_values = fit_curvature()
-    Image['curvature'] = curvature_values
-    print("Curvature is {} x^2 + {} x + {}".format(*Image['curvature']))
+        selected_image_name = '<<<SIMULATED>>>'
+        photon_events = make_fake_image()
+    curvature = fit_curvature(photon_events)
+    print("Curvature is {} x^2 + {} x + {}".format(*curvature))
 
     plt.figure()
     ax1 = plt.subplot(111)
+    plt.title('Image {}'.format(selected_image_name))
     plt.figure()
     ax2 = plt.subplot(111)
-    ax2.title('Spectrum {}'.format(Image['name']))
+    plt.title('Spectrum {}'.format(selected_image_name))
 
-    plot_image(ax1)
-    spectrum = extract()
-    plot_curvature(ax1)
-    resolution_values = fit_resolution()
-    plot_resolution(ax2)
-    plot_resolution_fit(ax2, resolution_values)
+    image_artist = plot_image(ax1, photon_events)
+    spectrum = extract(photon_events, curvature)
+    plot_curvature(ax1, curvature, photon_events)
+    resolution = fit_resolution(spectrum)
+    plot_resolution(ax2, spectrum)
+    plot_resolution_fit(ax2, spectrum, resolution)
 
-    print("Resolution is {}".format(resolution_values[0]))
+    print("Resolution is {}".format(resolution[0]))
     return ax1, ax2
 
 if __name__ == "__main__":
